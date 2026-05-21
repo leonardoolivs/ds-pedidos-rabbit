38d93d9a725475dde1c4394204b6a186e8ed1bac
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-20 22:07:52 -0300

    feat: Cria entity de Pedido

---
+++ src/main/java/com/rabbit/entities/Pedido.java
@@ -0,0 +1,21 @@
+package com.rabbit.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cliente;
+    private Double valorTotal;
+    private String emailNotificacao;
+}
