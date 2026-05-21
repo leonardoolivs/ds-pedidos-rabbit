829b82511cb6f1ef1e686eda9fee7fd40054d26c
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-20 21:59:34 -0300

    build: Cria o projeto com Lombok, RabbitMQ, Spring Web, Spring Data JPA

---
+++ src/main/java/com/rabbit/DsPedidosRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPedidosRabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsPedidosRabbitApplication.class, args);
+    }
+
+}
