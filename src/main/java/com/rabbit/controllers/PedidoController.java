f44408668a896696a35b99c0b545d591cda132a1
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-22 01:06:53 -0300

    fix: Configura Docker e application.properties para ir na porta certa de RaabitMQ

--- src/main/java/com/rabbit/controllers/PedidoController.java
+++ src/main/java/com/rabbit/controllers/PedidoController.java
@@ -1,6 +1,8 @@
 package com.rabbit.controllers;
 
 import com.rabbit.entities.Pedido;
+import com.rabbit.service.PedidoService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,14 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/pedidos")
 public class PedidoController {
 
-    public final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+    private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+
+    private final PedidoService service;
 
     @PostMapping
     public ResponseEntity<Pedido> cadastrar(@RequestBody Pedido pedido){
         logger.info("Pedido recebido {}", pedido.toString());
+        pedido = service.enfileirarFila(pedido);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
     }
